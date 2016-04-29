b82b6cbbac6019ab60e4011bfedd080f74bbd6cd
waitig <waitig@hotmail.com>
2016-04-29 11:42:09 +0800

    delete dist and build

--- complie.bat
+++ complie.bat
@@ -1,1 +1,2 @@
-python D:\PyInstaller\pyinstaller.py  -w  --onefile --icon="favico.ico" E:\pycode\tumblr\Tumload.py
+python D:\PyInstaller\pyinstaller.py  -w  --onefile --icon="favico.ico" E:\pycode\Tumload\Tumload.py
+pause
